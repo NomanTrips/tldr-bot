010d931f2bf62b38da8818dbc3ff21ef9cbbd881
NomanTrips <brianscott0017@yahoo.com>
2021-12-24 16:28:55 -0500

    First commit

--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
-browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+browser.runtime.onMessage.addListener(function () {
   console.log('Hello from the background')
 
   browser.tabs.executeScript({
--- src/popup/main.js
+++ src/popup/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './bootstrap.bundle.min.js';
+import './bootstrap.min.css';
 
 /* eslint-disable no-new */
 new Vue({
