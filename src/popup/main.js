957c567b44c4398bdb8f4066f1d6a01206f14bb0
NomanTrips <brianscott0017@yahoo.com>
2021-12-31 23:17:28 -0500

    integration w backend

--- src/popup/main.js
+++ src/popup/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './bootstrap.bundle.min.js';
-import './bootstrap.min.css';
+import './bootstrap.bundle.min.js'
+import './bootstrap.min.css'
+// Vue.prototype.$window = window
+// Vue.prototype.$appName = window
+// Vue.prototype.$thisTabs = browser.tabs
+Vue.prototype.$chromeBrowser = chrome
 
 /* eslint-disable no-new */
 new Vue({
